1e2c8d28623e9e8684ceea1918fe77c49c6d57bf
Evan Digby <evan.digby@go2mobi.com>
2015-09-14 08:57:44 -0700

    Starting debugger

--- debug/debug.go
+++ debug/debug.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/evandigby/nesgo/clock"
 	"github.com/evandigby/nesgo/cpu"
@@ -44,9 +45,73 @@ func (d *Debugger) cpu(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, string(json))
 }
 
+func (d *Debugger) memory(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	b := int64(0)
+	e := int64(len(d.cpuState.Memory))
+
+	if start, ok := r.URL.Query()["start"]; ok && len(start) > 0 {
+		parsed, err := strconv.ParseInt(start[0], 16, 64)
+		if err == nil && parsed > 0 && parsed < e {
+			b = parsed
+		}
+	}
+
+	if end, ok := r.URL.Query()["end"]; ok && len(end) > 0 {
+		parsed, err := strconv.ParseInt(end[0], 16, 64)
+		if err == nil && parsed > b && parsed < e {
+			e = parsed
+		}
+	}
+
+	json, err := json.Marshal(d.cpuState.Memory[b:e])
+
+	if err != nil {
+		d.writeError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, string(json))
+}
+
+func (d *Debugger) stack(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	b := int64(0)
+	e := int64(len(d.cpuState.Stack))
+
+	if start, ok := r.URL.Query()["start"]; ok && len(start) > 0 {
+		parsed, err := strconv.ParseInt(start[0], 16, 64)
+		if err == nil && parsed > 0 && parsed < e {
+			b = parsed
+		}
+	}
+
+	if end, ok := r.URL.Query()["end"]; ok && len(end) > 0 {
+		parsed, err := strconv.ParseInt(end[0], 16, 64)
+		if err == nil && parsed > b && parsed < e {
+			e = parsed
+		}
+	}
+
+	json, err := json.Marshal(d.cpuState.Stack[b:e])
+
+	if err != nil {
+		d.writeError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, string(json))
+}
+
 func (d *Debugger) serve() {
 	http.Handle("/ui/", http.StripPrefix("/ui", http.FileServer(http.Dir(d.uiFolder))))
 	http.HandleFunc("/cpu", d.cpu)
+	http.HandleFunc("/memory", d.memory)
+	http.HandleFunc("/stack", d.memory)
 
 	http.ListenAndServe(":9905", nil)
 }
